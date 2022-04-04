b788cb7db7d5f27a358e893cea980c22662dc0d0
Đạt Nguyễn Thành <19dh111172@st.huflit.edu.vn>
2022-04-04 21:53:09 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Hello World with reactjs for thanhdat
         </p>
         <a
           className="App-link"
